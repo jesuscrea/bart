3b7fe96dd48517f0d0956f502729602940901150
jesus <jbrdev21880949243098@protonmail.com>
2020-06-06 03:56:05 +0200

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/index.scss';
+import { APP_NAME } from './core/config/constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <h1>{APP_NAME}</h1>
+  </div>
+);
 
 export default App;
