7b262dc9aca8aeca40f39a784a364f951f3328e0
jesus <jbrdev21880949243098@protonmail.com>
2020-06-14 19:38:46 +0200

    header changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { Title, Link } from '.';
-import { ARTIST } from '../core/config/constants';
-import { capitalize } from '../core/utils';
+import { Title } from '.';
 import logo from '../assets/images/logo.png';
+import { ARTIST } from '../core/config/constants';
 
 const Header: React.FC = () => (
   <div className="Header">
-    <div className="Header__pre">
-      <img className="Header__logo" src={logo} alt="logo" />
-      <Title className="Header__Title" title={ARTIST.TITLE} />
-    </div>
-    <div className="Header__description">
-      {ARTIST.DESCRIPTION.map((el: string, index: any) => (
-        <Link
-          key={`description-link-${index}`}
-          className="pr--2"
-          href={'#'}
-          text={capitalize(el)}
-        />
-      ))}
-    </div>
+    <img className="Header__logo" src={logo} alt="logo" />
+    <Title className="Header__Title" title={ARTIST.TITLE} />
   </div>
 );
 
