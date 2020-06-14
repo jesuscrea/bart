cbf42f11c6073786da860a7ae33d2a390075452b
jesus <jbrdev21880949243098@protonmail.com>
2020-06-14 19:37:56 +0200

    add spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import spinner from '../assets/images/spinner.svg';
+
+const Spinner = () => <img className="Spinner" src={spinner} alt="spinner" />;
+
+export default Spinner;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,7 @@ import Carousel from './Carousel';
 import Footer from './Footer';
 import Header from './Header';
 import Link from './Link';
+import Spinner from './Spinner';
 import Title from './Title';
 
-export { Link, Footer, Carousel, Header, Title };
+export { Link, Footer, Carousel, Header, Title, Spinner };
